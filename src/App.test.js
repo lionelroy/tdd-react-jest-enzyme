9f0be7ccf39308fcfaa652f5be7bdff1e793b151
Stephane Roy <stephroy411@hotmail.com>
2022-01-02 13:29:39 -0400

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
 import App from './App';
+import { shallow } from 'enzyme';
+import PersonList from './PersonList';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+describe('App', () => {
+  it('renders without crashing', () => {
+    const appWrapper = shallow(<App />)
+  });
+
+  it('', () => {
+    const appWrapper = shallow(<App />);
+    const personList = appWrapper.find(PersonList);
+  
+    expect(personList).toHaveLength(1);
+  })
+})
